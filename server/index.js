55359153035e10da31c1f64aa469bd1d15ea007f
michaellosdev <mishaloskutov@gmail.com>
2022-12-30 12:11:58 -0800

    build in public

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(logger)
 app.use(cookieParser())
 app.use(cors(corsOptions))
 app.use(function (req, res, next) {	
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
+    res.setHeader('Access-Control-Allow-Origin', 'https://sampledash.onrender.com');    
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
     res.setHeader('Access-Control-Allow-Credentials', true);    
